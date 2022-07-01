6e5880e917c99321a68ffa4c5b170b1793148991
ramiinberg <rami.inberg@gmail.com>
2022-07-01 10:42:31 +0300

    saving deckId to local variable

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+let deckId
+
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        deckId = data.deck_id
+      })
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
