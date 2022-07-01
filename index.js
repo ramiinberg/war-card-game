3768cefa1250b9083721f73f21bcdd19f3d8e783
ramiinberg <rami.inberg@gmail.com>
2022-07-01 10:57:02 +0300

    Added draw cards button.

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ function handleClick() {
       .then(res => res.json())
       .then(data => {
         deckId = data.deck_id
+        document.getElementById("draw-cards").disabled = false;
       })
 }
 
+function handleDrawTwoCards() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+      console.log('data', data)
+    })
+}
+
 document.getElementById("new-deck").addEventListener("click", handleClick)
+document.getElementById("draw-cards").addEventListener("click", handleDrawTwoCards)
